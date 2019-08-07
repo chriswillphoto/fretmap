46640a314cf6bb892d17ffb5257ea5682b20881a
Christopher Nekich <chriswilldev@gmail.com>
2019-08-07 14:45:33 +1000

    string notes for all 6 strings in e standard

--- plugins/utils.js
+++ plugins/utils.js
@@ -5,7 +5,7 @@ fretLength = 22
 var noteIterator = function(i, zero) {
   var notes = [ 'A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab' ]
   var intervals = ['Tonic', 'minor 2nd', 'Major 2nd', 'minor 3rd', 'Major 3rd', 'Perfect 4th', 'diminished 5th', 'Perfect 5th', 'minor 6th', 'Major 6th', 'minor 7th', 'Major 7th']
-  var abbreviatedIntervals = ['1', '-2', '2', '-3', '3', '4', '-5', '5', '-6', '6', '-7', '7']
+  var abbreviatedIntervals = ['1', '-2', '+2', '-3', '+3', '4', '-5', '5', '-6', '+6', '-7', '+7']
   var zeroIndex = notes.indexOf(zero)
   var adjustedNotes = [...notes.slice(zeroIndex), ...notes.slice(0, zeroIndex)]
   var fretIndex = i
@@ -20,10 +20,21 @@ var noteIterator = function(i, zero) {
   }
 }
 
-var E = []
+var strings = {}
+strings.E = []
+strings.A = []
+strings.D = []
+strings.G = []
+strings.B = []
+strings.highE = []
 
 for(i = 0; i <= fretLength; i++){
-  E.push(noteIterator(i, 'E'))
+  strings.E.push(noteIterator(i, 'E'))
+  strings.A.push(noteIterator(i, 'A'))
+  strings.D.push(noteIterator(i, 'D'))
+  strings.G.push(noteIterator(i, 'G'))
+  strings.B.push(noteIterator(i, 'B'))
+  strings.highE.push(noteIterator(i, 'E'))
 }
 
-console.log(E)
+console.log(strings)
