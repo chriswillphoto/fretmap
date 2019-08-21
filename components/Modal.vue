8ebea9dc25fa6f57fb057cb5d763b91127b06f63
Christopher Nekich <chriswilldev@gmail.com>
2019-08-21 16:37:06 +1000

    added download svg to download button

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="modal-container" @click="closeModal" :class="showModal ? 'show' : 'hide'">
-    <img src="" alt="" id="modal">
-    <a href="" id="downloadButton" @click='download' download='Boardwalk.png'>Download</a>
+
+    <div class="image-container">
+      <img src="" alt="" id="modal">
+      <a href="" id="downloadButton" @click='download' download='Boardwalk.png'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z" fill="#FFFFFF"/></svg></a>
+    </div>
   </div>
 </template>
 
@@ -42,11 +45,15 @@ export default {
     display: none;
   }
 
-  #modal {
+  .image-container {
     max-width: 80vw;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+  }
+
+  #modal {
+    width: 100%;
   }
 </style>
