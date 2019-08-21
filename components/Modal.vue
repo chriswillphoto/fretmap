f050db7b8f84670418d8065ba6cf6d6f19e75118
Christopher Nekich <chriswilldev@gmail.com>
2019-08-21 16:44:34 +1000

    added modal close button

--- components/Modal.vue
+++ components/Modal.vue
@@ -2,6 +2,7 @@
   <div class="modal-container" @click="closeModal" :class="showModal ? 'show' : 'hide'">
 
     <div class="image-container">
+      <button class="close" @click='closeModal'>X</button>
       <img src="" alt="" id="modal">
       <a href="" id="downloadButton" @click='download' download='Boardwalk.png'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z" fill="#FFFFFF"/></svg></a>
     </div>
@@ -43,6 +44,12 @@ export default {
 
   .modal-container.hide {
     display: none;
+  }
+
+  .close {
+    position: absolute;
+    bottom: 100%;
+    right: 0;
   }
 
   .image-container {
