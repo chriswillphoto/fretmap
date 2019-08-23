4bc3008bcea17ea1837eccc2542e5322ebd1f447
Christopher Nekich <chriswilldev@gmail.com>
2019-08-23 15:46:24 +1000

    added appropriate filename to screenshot download

--- components/Modal.vue
+++ components/Modal.vue
@@ -4,7 +4,7 @@
     <div class="image-container">
       <button class="close" @click='closeModal'>X</button>
       <img src="" alt="" id="modal">
-      <a href="" id="downloadButton" @click='download' download='Boardwalk.png'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z" fill="#FFFFFF"/></svg></a>
+      <a href="" id="downloadButton" @click='download' :download='activeKey + activeMode + ".png"'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z" fill="#FFFFFF"/></svg></a>
     </div>
   </div>
 </template>
@@ -27,6 +27,15 @@ export default {
   computed: {
     showModal(){
       return this.$store.state.showModal
+    },
+    activeKey(){
+      return this.$store.state.activeKey
+    },
+    activeMode(){
+      return this.$store.state.activeMode
+    },
+    pentatonic(){
+      return this.$store.state.pentatonic
     }
   }
 }
@@ -58,6 +67,18 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+  }
+
+  #downloadButton {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: green;
+  }
+
+  svg {
+    width:45px;
+    height: 45px;
   }
 
   #modal {
