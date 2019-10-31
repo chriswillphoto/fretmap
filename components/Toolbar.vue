0c140ee77d61c8bf8cb0e8c226a94239955174e8
Christopher Nekich <chriswilldev@gmail.com>
2019-10-31 14:52:21 +1100

    added sass, added toolbar title

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -27,18 +27,19 @@
       <div class="pentatonic" :class='"pentatonic-"+pentatonic'>
         <button @click='togglePentatonic' class='label' :class='"pentatonic-"+pentatonic'>Pentatonic</button>
         <transition name="fade">
-        <svg v-if='pentatonic' class='checkmark' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ff0000" stroke="#000"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
+        <svg v-if='pentatonic' class='checkmark' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#FAA2FF" stroke="#000"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
         </transition>
       </div>
     </div>
     
     <div class="view-options">
+      <h3>Set Fret View</h3>
       <div class="lower-view">
-        <label for="lower-fret-view">Set fret view - Low</label>
+        <label for="lower-fret-view">Low</label>
         <input name='lower-fret-view' type="number" min='0' :max='upperView' step='1' v-model='lowerView'>
       </div>
       <div class="higher-view">
-        <label for="higher-fret-view">Set fret view - High</label>
+        <label for="higher-fret-view">High</label>
         <input name='higher-fret-view' type="number" min='1' :max='fretNumber' step='1' v-model='upperView'>
       </div>
     </div>
@@ -141,8 +142,6 @@ export default {
         // console.log(canvas)
         this.$store.commit('showModal')
       })
-
-      
     }
   },
   mounted() {
@@ -151,7 +150,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
 
 .keysig {
   animation: fadeIn 0.5s linear forwards 1 ;
@@ -174,6 +173,8 @@ export default {
   width: 100%;
   height: 30vh;
   flex: 1;
+  max-width: 1100px;
+  margin: 0 auto;
 }
 
 .dropdowns {
@@ -189,9 +190,13 @@ export default {
 
 .view-options {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-row-gap: 12px;
   padding: 8px;
+
+  h3 {
+    grid-column: 1 / span 2;
+  }
 }
 
 .screenshot {
@@ -204,48 +209,49 @@ export default {
   font-size: 14px;
   font-weight: 600;
   margin-top: 12px;
+  margin-bottom: 12px;
 }
 
   .select-css {
-      display: block;
-      font-size: 16px;
-      font-family: sans-serif;
-      font-weight: 700;
-      color: #444;
-      line-height: 1.3;
-      padding: .6em 1.4em .5em .8em;
-      width: 140px;
-      max-width: 100%; 
-      box-sizing: border-box;
-      margin: 0 auto;
-      border: 1px solid #aaa;
-      box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
-      border-radius: .5em;
-      -moz-appearance: none;
-      -webkit-appearance: none;
-      appearance: none;
-      background-color: #fff;
-      background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-        linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
-      background-repeat: no-repeat, repeat;
-      background-position: right .7em top 50%, 0 0;
-      background-size: .65em auto, 100%;
+    display: block;
+    font-size: 16px;
+    font-family: sans-serif;
+    font-weight: 700;
+    color: #444;
+    line-height: 1.3;
+    padding: .6em 1.4em .5em .8em;
+    width: 140px;
+    max-width: 100%; 
+    box-sizing: border-box;
+    margin: 0 auto;
+    border: 1px solid #aaa;
+    box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+    border-radius: .5em;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: #fff;
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+      linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right .7em top 50%, 0 0;
+    background-size: .65em auto, 100%;
   }
   .select-css::-ms-expand {
-      display: none;
+    display: none;
   }
   .select-css:hover {
-      border-color: #888;
+    border-color: #888;
   }
   .select-css:focus {
-      border-color: #aaa;
-      box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
-      box-shadow: 0 0 0 3px -moz-mac-focusring;
-      color: #222; 
-      outline: none;
+    border-color: #aaa;
+    box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
+    box-shadow: 0 0 0 3px -moz-mac-focusring;
+    color: #222; 
+    outline: none;
   }
   .select-css option {
-      font-weight:normal;
+    font-weight:normal;
   }
 
   .label::selection {
